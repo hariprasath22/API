3e24c5c32716f2a9cff00e6c922e6880e34c24f2
deepocean2904 <deepikachu2904@gmail.com>
2025-01-11 19:03:24 -1100

    Add project files.

---
+++ API/EmailService.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace API
+{
+
+    public class EmailService
+    {
+        public EmailService(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void SendEmail(string toEmail, string subject, string body)
+        {
+            var fromEmail = Configuration.GetSection("Constants:FromEmail").Value ?? string.Empty;
+            var fromEmailPassword = Configuration.GetSection("Constants:EmailAccountPassword").Value ?? string.Empty;
+
+            if (!IsValidEmail(fromEmail) || !IsValidEmail(toEmail))
+            {
+                throw new FormatException("Invalid email address format.");
+            }
+
+            var message = new MailMessage()
+            {
+                From = new MailAddress(fromEmail),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+
+            message.To.Add(toEmail);
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(fromEmail, fromEmailPassword),
+                EnableSsl = true,
+            };
+
+            smtpClient.Send(message);
+        }
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new MailAddress(email);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
